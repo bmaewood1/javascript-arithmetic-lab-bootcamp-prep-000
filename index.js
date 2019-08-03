94d6317a0a0daf7e5e4b9b492b6186c10f1317c2
Bailey Wood <woodbmae@gmail.com>
2019-08-03 19:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,33 +9,49 @@ function add(a, b) {
   return a + b 
 }
 
+add(a, b)
+
 function subtract(a, b) {
   return a - b
 }
+
+subtract(a, b)
 
 function multiply(a, b) {
   return a * b
 }
 
+multiply(a, b)
+
 function divide(a, b) {
   return a / b
 }
+
+divide(a, b)
 
 function inc(n) {
   return a + 1
 }
 
+inc(a)
+
 function dec(n) {
   return a - 1
 }
 
+dec(a)
+
 function makeInt(n) {
   return parseInt("n")
 }
+
+makeInt(a)
   
 
 function preserveDecimal(n) {
   return parseFloat("n")
 }
+
+preserveDecimal(a)
 
 
